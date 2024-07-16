c661a1b60bd59f14602257206550acfd3a6f5969
darkBlueLemon <amithkchoran@gmail.com>
2024-07-16 22:51:33 +0530

    Confirmation on clear history

--- app/src/main/java/com/minimalisttodolist/pleasebethelastrecyclerview/ui/dialogs/HistoryDialog.kt
+++ app/src/main/java/com/minimalisttodolist/pleasebethelastrecyclerview/ui/dialogs/HistoryDialog.kt
@@ -42,29 +42,35 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.minimalisttodolist.pleasebethelastrecyclerview.data.model.DeletedTask
+import com.minimalisttodolist.pleasebethelastrecyclerview.data.model.PriorityColor
 import com.minimalisttodolist.pleasebethelastrecyclerview.ui.components.CustomBox
 import com.minimalisttodolist.pleasebethelastrecyclerview.ui.components.CustomDropdownMenu
+import com.minimalisttodolist.pleasebethelastrecyclerview.ui.theme.LocalDarkTheme
 import com.minimalisttodolist.pleasebethelastrecyclerview.util.vibrate
 import com.minimalisttodolist.pleasebethelastrecyclerview.viewmodel.AppEvent
 import com.minimalisttodolist.pleasebethelastrecyclerview.viewmodel.TaskEvent
 import com.minimalisttodolist.pleasebethelastrecyclerview.viewmodel.TaskViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -121,20 +127,21 @@ fun HistoryDialog(
 @Composable
 fun TitleAndDeleteAll(onClearHistory: () -> Unit, context: Context, onBack: () -> Unit) {
     var expanded by remember { mutableStateOf(false) }
+    var confirmDelete by remember { mutableStateOf(false) }
+    val confirmationTextColor =  PriorityColor.PRIORITY3.getColor(LocalDarkTheme.current)
 
-    Box (
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(IntrinsicSize.Max)
             .padding(8.dp)
-    ){
+    ) {
         Icon(
             imageVector = Icons.Rounded.ArrowBack,
             contentDescription = "back arrow",
             modifier = Modifier
                 .align(Alignment.CenterStart)
                 .fillMaxHeight()
-//                .padding(start = 12.dp, end = 12.dp)
                 .clickable {
                     onBack()
                 }
@@ -146,18 +153,24 @@ fun TitleAndDeleteAll(onClearHistory: () -> Unit, context: Context, onBack: () -
             modifier = Modifier.align(Alignment.Center)
         )
         Text(
-            text = "Clear",
+            text = if (confirmDelete) "Confirm?" else "Clear",
             style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.primary,
+            color = if (confirmDelete) confirmationTextColor else MaterialTheme.colorScheme.primary,
+            fontWeight = if (confirmDelete) FontWeight.Bold else FontWeight.Normal,
             modifier = Modifier
                 .align(Alignment.CenterEnd)
                 .clickable {
-                    onClearHistory()
+                    if (confirmDelete) {
+                        onClearHistory()
+                        confirmDelete = false
+                    } else {
+                        confirmDelete = true
+                    }
                     expanded = false
                     vibrate(context = context, strength = 1)
                 }
                 .fillMaxHeight()
-                .wrapContentHeight(Alignment.CenterVertically) // This centers the text vertically
+                .wrapContentHeight(Alignment.CenterVertically)
         )
     }
 }
